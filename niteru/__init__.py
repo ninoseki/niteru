05770c2f410eff58c11b4f0cb136ea983bee594e
Manabu Niseki <manabu.niseki@gmail.com>
2021-05-06 17:59:26 +0900

    refactor: use poetry_version

--- niteru/__init__.py
+++ niteru/__init__.py
@@ -1,10 +1,13 @@
 """
 .. include:: ../README.md
 """
+import poetry_version
+
 from niteru.similarity import similarity
 from niteru.structural_similarity import structural_similarity
 from niteru.style_similarity import style_similarity
 
-__version__ = "0.1.1"
+__version__ = str(poetry_version.extract(source_file=__file__))
+
 
 __all__ = ["structural_similarity", "style_similarity", "similarity"]
